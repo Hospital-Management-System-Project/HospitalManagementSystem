c96727fb39dd927d80ce67da12380525cae35644
Cam Vandy <88672691+camvandy@users.noreply.github.com>
2025-04-01 21:25:00 -0400

    Completed the drugs.h file update

--- drugs.h
+++ drugs.h
@@ -3,22 +3,25 @@
 
 #include <string>
 
+// Drug class to represent a drug with its name and price
 class drug {
+// Encapsulated data members
 private:
     std::string drugName;
     double price;
 
+// Public member functions
 public:
-    drug(const std::string& name, double p) : drugName(name), price(p) {}
-
+    drug(const std::string& name, double p){
+        drugName = name;
+        price = p;
+    }
     // Getters
     std::string getDrugName() const {
         return drugName;
     }
-
     double getPrice() const {
         return price;
     }
 };
-
 #endif // DRUG_H
