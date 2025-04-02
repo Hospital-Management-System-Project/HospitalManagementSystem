c3b68e158e2cc0a69ff2a2c364e44162283f2561
Cam Vandy <88672691+camvandy@users.noreply.github.com>
2025-04-01 21:33:05 -0400

    Completed the nurse.h and cpp file updated

--- nurse.cpp
+++ nurse.cpp
@@ -4,32 +4,32 @@
 
 using namespace std;
 
+// We implement the constructor and member functions of the Nurse class here
 Nurse::Nurse(string id, string name, string hospID) {
   nurseID = id;
   nurseName = name;
   hospitalID = hospID;
 }
 
+// Getter and setter methods for the Nurse class
 string Nurse::getNurseID() const {
     return nurseID;
 }
-
 string Nurse::getNurseName() const {
     return nurseName;
 }
-
 string Nurse::getHospitalID() const {
     return hospitalID;
 }
-
+// We have a getter for the hospitalID, it grabs the patientIDs from the vector
 vector<string> Nurse::getPatientIDs() const {
     return patientIDs;
 }
-
 void Nurse::setHospitalID(string hosID) {
     this->hospitalID = hosID;
 }
 
+// We have a function that assigns a patient to the nurse
 bool Nurse::assignPatient(string patientID) {
   if (patientIDs.size() < 2) {
     patientIDs.push_back(patientID);
@@ -38,18 +38,18 @@ bool Nurse::assignPatient(string patientID) {
   return false;
 }
 
+// We have a function that removes a patient from the nurse
 void Nurse::removePatient(string patientID) {
   patientIDs.erase(remove(patientIDs.begin(), patientIDs.end(), patientID), patientIDs.end());
 }
 
+// This function will get the full description of the nurse
 string Nurse::getFullDescription() const {
     stringstream description;
-
     // Nurse info
     description << "=== NURSE DETAILS ===\n";
     description << "Nurse ID: " << getNurseID() << "\n";
     description << "Name: " << getNurseName() << "\n";
-
     // Assigned patients
     description << "=== NURSE'S PATIENTS ===\n";
     if (!getPatientIDs().empty()) {
@@ -62,8 +62,7 @@ string Nurse::getFullDescription() const {
         }
         description << "\n";
     } else {
-        description << "No assigned patients at this time.\n";
+        description << "No Assigned Patients At This Time!" endl;
     }
-
     return description.str();
 }
--- nurse.h
+++ nurse.h
@@ -3,30 +3,32 @@
 
 #include <string>
 #include <vector>
+using namespace std;    // Using standard namespace for simplicity
 
-using namespace std;
-
+// We created a Nurse class to manage nurse information and their assigned patients
 class Nurse {
+// Private member variables to store nurse information
 private:
     string nurseID;
     string nurseName;
     string hospitalID;
-    vector<string> patientIDs;
+    vector<string> patientIDs;  // Vector to store IDs of assigned patients
 
+// Public member functions to manage nurse information
 public:
     Nurse(string id, string name, string hospID);
 
-    // Getters
+    // Getters for nurse information
     string getNurseID() const;
     string getNurseName() const;
     string getHospitalID() const;
-    vector<string> getPatientIDs() const;
+    vector<string> getPatientIDs() const;   // Vector of patient IDs that the nurse is assigned to
     string getFullDescription() const;
 
-    // Setters
+    // Setters for setting the hospital ID for the nurse
     void setHospitalID(string hosID);
 
-    // Patient assignment
+    // Patient Assignment and Removal for the nurse
     bool assignPatient(string patientID);
     void removePatient(string patientID);
 };
