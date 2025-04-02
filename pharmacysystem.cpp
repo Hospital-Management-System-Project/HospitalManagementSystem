77c1d0ad17e21cf4b673ea1b8fb438bdd5129153
Cam Vandy <88672691+camvandy@users.noreply.github.com>
2025-04-01 21:59:46 -0400

    Completed pharmacysystem.h and cpp file Updated

--- pharmacysystem.cpp
+++ pharmacysystem.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-// Initialize the static instance pointer
+// Initialize the static instance pointer which will be used to manage the singleton instance
 PharmacySystem* PharmacySystem::instance = nullptr;
 
 void PharmacySystem::initializePharmacies() {
@@ -14,7 +14,7 @@ void PharmacySystem::initializePharmacies() {
     }
     pharmacies.clear();
     
-    // Pharmacy names - examples from Windsor, ON area
+    // We generated pharmacy names to meet the requirement of 20 pharmacies
     const string PHARMACY_NAMES[20] = {
         "Shoppers Drug Mart", "Rexall Pharmacy", "Walmart Pharmacy", 
         "Medical Arts Pharmacy", "Loblaw Pharmacy", "Erie Pharmacy",
@@ -27,47 +27,51 @@ void PharmacySystem::initializePharmacies() {
     
     // Create 20 pharmacies with unique IDs
     for (int i = 0; i < 20; i++) {
-        string id = "P" + to_string(i + 1);
+        string id = "P" + to_string(i + 1); // We put the pharmacy ID in the format P1, P2, ..., P20
         pharmacies.push_back(new Pharmacy(id, PHARMACY_NAMES[i]));
     }
 }
 
+// We have a function to initialize the drugs
 void PharmacySystem::initializeDrugs() {
-    drugList.clear();
-    drugList.push_back(drug("Aspirin", 10.0));
+    drugList.clear();   // Start by clearing the existing drug list
+    // We added a couple drugs with their prices to the drug list vector using push_back
+    drugList.push_back(drug("Aspirin", 10.0)); 
     drugList.push_back(drug("Ibuprofen", 12.5));
     drugList.push_back(drug("Acetaminophen", 8.0));
     drugList.push_back(drug("Amoxicillin", 25.0));
     drugList.push_back(drug("Insulin", 50.0));
 }
 
+// We have a function to get the pharmacy by index
 Pharmacy* PharmacySystem::getPharmacy(int index) {
     if (index >= 0 && static_cast<size_t>(index) < pharmacies.size()) {
-        return pharmacies[index];
+        return pharmacies[index];   // Return the pharmacy at the given index
     }
     return nullptr;
 }
 
+// We have a function to get all the pharmacies in the vector
 vector<Pharmacy*> PharmacySystem::getAllPharmacies() {
     return pharmacies;
 }
 
+// We have a function to get the pharmacy status which will show the status of all pharmacies
 string PharmacySystem::getPharmacyStatus() {
     stringstream status;
-    
-    status << "=== Pharmacy System Status ===\n\n";
-    
+    status << "=== Pharmacy System Status ===\n\n"; 
+    // We have a loop to iterate through all pharmacies and get their status
     for (auto pharmacy : pharmacies) {
         status << "Pharmacy: " << pharmacy->getPharmacyName() << " (ID: " << pharmacy->getPharmacyID() << ")\n";
         status << "Pending Payments: $" << pharmacy->getPendingPayments() << "\n\n";
     }
-    
-    return status.str();
+    return status.str();    // Return the status as a string
 }
 
+// Destructor to clean up the pharmacies and free memory
 PharmacySystem::~PharmacySystem() {
     for (auto pharmacy : pharmacies) {
         delete pharmacy;
     }
-    pharmacies.clear();
+    pharmacies.clear();     
 }
--- pharmacysystem.h
+++ pharmacysystem.h
@@ -7,33 +7,41 @@
 #include "pharmacy.h"
 #include "drugs.h"
 
-using namespace std;
+using namespace std;    // We used the using directive to avoid typing std:: everywhere
 
+// Singleton class to manage the pharmacy system
 class PharmacySystem {
 private:
-    static PharmacySystem* instance;
-    vector<Pharmacy*> pharmacies;
-    vector<drug> drugList; 
-    
-    // Private constructor
-    PharmacySystem() {}
+    static PharmacySystem* instance;    // Singleton instance to manage the pharmacy system
+    vector<Pharmacy*> pharmacies;       // This is the vector of pharmacies that will be used
+    vector<drug> drugList;              // This is the vector of drugs that will be used
+
+    PharmacySystem() {}                 // Private constructor for singleton
 
 public:
+    // Static method to get the singleton instance which will be used to access the pharmacy system
     static PharmacySystem* getInstance() {
-        if (!instance) {
-            instance = new PharmacySystem();
+        if (!instance) {    // If the instance is not created yet
+            instance = new PharmacySystem();    // Then create it 
         }
-        return instance;
+        return instance;    // Return the instance of the pharmacy system
     }
     
+    // Initialize the pharmacies and drugs
     void initializeDrugs();
     void initializePharmacies();
+
+    // Getters for pharmacies and drugs
     Pharmacy* getPharmacy(int index);
     vector<Pharmacy*> getAllPharmacies();
-    std::vector<drug> getAllDrugs() const { return drugList; }
-        
-    string getPharmacyStatus();
-    ~PharmacySystem();
-};
 
+    // Getters for drugs as a vector which will be used to get all the drugs
+    std::vector<drug> getAllDrugs() const { 
+        return drugList; 
+    }
+    
+    // Getters for the pharmacy status
+    string getPharmacyStatus();   // Get the status of the pharmacy system
+    ~PharmacySystem();   // Destructor to clean up the pharmacies
+};
 #endif // PHARMACYSYSTEM_H
