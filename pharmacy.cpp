2a8758189f40c9bdfc6accdb85762d529cde4f3c
Cam Vandy <88672691+camvandy@users.noreply.github.com>
2025-04-01 21:46:42 -0400

    Completed pharmacy.h and .cpp file updated

--- pharmacy.cpp
+++ pharmacy.cpp
@@ -3,57 +3,59 @@
 
 using namespace std;
 
-Pharmacy::Pharmacy(string id, string n) {
+// Define the constructor for the Pharmacy class with id and name
+Pharmacy::Pharmacy(string id, string name) {
     pharmacyID = id;
-    pharmacyName = n;
+    pharmacyName = name;
     address = "";
     phoneNumber = "";
 }
 
-Pharmacy::Pharmacy(string id, string n, string addr, string phone) {
+// Define the constructor for the Pharmacy class with id, name, address, and phone
+Pharmacy::Pharmacy(string id, string name, string addr, string phone) {
     pharmacyID = id;
-    pharmacyName = n;
+    pharmacyName = name;
     address = addr;
     phoneNumber = phone;
 }
 
+// Define the getters for the Pharmacy class
 string Pharmacy::getPharmacyID() const {
     return pharmacyID;
 }
-
 string Pharmacy::getPharmacyName() const {
     return pharmacyName;
 }
 
+// Define the function to get the total amount of pending payments
 double Pharmacy::getPendingPayments() const {
-    double total = 0.0;
-    for (const auto& b : bills) {
-        if (!b.paid) {
-            total += b.amount;
+    double totalAmt = 0.0;
+    for (const auto& bill : bills) {    // Loop through all the bills
+        if (!bill.paid) {   // Check if the bill is unpaid
+            totalAmt += bill.amount;    // Add the amount to the total
         }
     }
-    return total;
+    return totalAmt;
 }
 
+// Define the function to get all bills for a specific hospital
 vector<Bill> Pharmacy::getBillsForHospital(string hospitalID) const {
-    vector<Bill> hospitalBills;
-    for (const auto& b : bills) {
-        if (b.hospitalID == hospitalID) {
-            hospitalBills.push_back(b);
+    vector<Bill> hospitalBills;         // Vector to store bills for the hospital
+    for (const auto& bill : bills) {    // Loop through all the bills
+        if (bill.hospitalID == hospitalID) {
+            hospitalBills.push_back(bill);  // Add the bill to the vector if it belongs to the hospital
         }
     }
     return hospitalBills;
 }
 
+// Define the function to bill a hospital for a drug
 string Pharmacy::billHospitalForDrug(string hospitalID, string drugName, double price) {
-    string sanitizedDrugName = drugName;
-    replace(sanitizedDrugName.begin(), sanitizedDrugName.end(), ' ', '_');
-    
-    string billID = "DRUGBILL-" + pharmacyID + "-" + hospitalID + "-" + 
-                    sanitizedDrugName + "-" + to_string(rand() % 100000);
-
-    // Add bill
+    string formattedDrugName = drugName;
+    replace(formattedDrugName.begin(), formattedDrugName.end(), ' ', '_');
+    // Generate a unique bill ID
+    string billID = "DRUGBILL-" + pharmacyID + "-" + hospitalID + "-" + formattedDrugName + "-" + to_string(rand() % 100000);
+    // We will create a new bill and add it to the vector
     bills.push_back(Bill(billID, hospitalID, price));
-
     return billID;
 }
--- pharmacy.h
+++ pharmacy.h
@@ -4,30 +4,36 @@
 #include <string>
 #include <vector>
 
-using namespace std;
+using namespace std;    // We used standard namespace for convenience
 
-// Structure to represent a bill
+// Created a struct data structure to represent a bill to the pharmacy/patient
 struct Bill {
     string billID;
     string hospitalID;
     double amount;
     bool paid;
 
-    Bill(string id, string hospID, double amt)
-        : billID(id), hospitalID(hospID), amount(amt), paid(false) {}
-};
+    // Constructor for Bill
+    Bill(string id, string hospID, double amt) {
+        billID = id;
+        hospitalID = hospID;
+        amount = amt;
+        paid = false;
+    }
+};;
 
+// Created a class to represent a pharmacy
 class Pharmacy {
 private:
     string pharmacyID;
     string pharmacyName;
     string address;
     string phoneNumber;
-    vector<Bill> bills;
+    vector<Bill> bills; // Created a vector to store bills
 
 public:
-    Pharmacy(string id, string n);
-    Pharmacy(string id, string n, string addr, string phone);
+    Pharmacy(string id, string name);
+    Pharmacy(string id, string name, string addr, string phone);
 
     // Getters
     string getPharmacyID() const;
@@ -36,9 +42,9 @@ public:
     // Billing functions
     string billHospitalForDrug(string hospitalID, string drugName, double price);
 
-    // Reporting
+    // This function will be used to grab the total amount of pending payments
     double getPendingPayments() const;
-    vector<Bill> getBillsForHospital(string hospitalID) const;
+    vector<Bill> getBillsForHospital(string hospitalID) const;  // Created a vector to get bills for a hospital
 };
 
 #endif // PHARMACY_H
